9433e1ff6f22f748a048bacd93fd6f6083d130d1
mhmmdabii <bii.200524@gmail.com>
2024-09-20 11:11:33 +0700

    Add files via upload

---
+++ abii 1.py
@@ -0,0 +1,9 @@
+#penginputan 
+nama1 = (input("nama kamu siapa: "))
+asal1 = (input("dari mana asal kamu?: "))
+sekolah1 = (input("sebelumnya sekolah dimana: "))
+
+#output
+print ("halo selamat siang",nama1)
+print ("dari mana asal kamu",asal1)
+print ("sebelumnya bersekolah dimana",sekolah1)
